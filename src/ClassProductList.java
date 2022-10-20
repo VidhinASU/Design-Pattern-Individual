b9cee827adc620d58a2877877ff537170a8146ed
VidhinASU <112655368+VidhinASU@users.noreply.github.com>
2022-10-20 13:55:13 -0700

    Upload files generated by UML

---
+++ src/ClassProductList.java
@@ -0,0 +1,13 @@
+public class ClassProductList extends ArrayList {
+
+	private ProductIterator productIterator;
+
+	private Product[] product;
+
+	private ReminderVisitor reminderVisitor;
+
+	public void accept(NodeVisitor visitor) {
+
+	}
+
+}
---
+++ src/OfferingList.java
@@ -0,0 +1,11 @@
+public class OfferingList extends ArrayList {
+
+	private OfferingIterator offeringIterator;
+
+	private Trading trading;
+
+	private OfferingList offeringList;
+
+	private Offering[] offering;
+
+}
